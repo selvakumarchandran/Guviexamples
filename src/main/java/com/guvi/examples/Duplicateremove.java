a8fee1c8ca29980551b6703dc5c1ddce4fb73267
Maintwizdev <selvakumarcjava@gmail.com>
2018-01-25 18:18:48 +0530

    second commit

--- src/main/java/com/guvi/examples/Duplicateremove.java
+++ src/main/java/com/guvi/examples/Duplicateremove.java
@@ -1,4 +1,30 @@
 package com.guvi.examples;
 
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
 public class Duplicateremove {
+
+
+    public static Set getValues()
+    {
+        Scanner scanner = new Scanner(System.in);
+        Set list = new HashSet();
+        Integer firstTot = Integer.valueOf(scanner.nextLine());
+        String[] secondaryvalue = scanner.nextLine().split(" ");
+        for (int i = 0; i <secondaryvalue.length ; i++) {
+            list.add(secondaryvalue[i]);
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        Set list = new HashSet();
+        list = getValues();
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext())
+        {
+            System.out.print(iterator.next());
+        }
+        }
 }
